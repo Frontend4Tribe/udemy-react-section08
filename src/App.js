ebad88421936b12604cc4e3b7e8f717c753141b7
RearrangeWorld <80311827+RearrangeWorld@users.noreply.github.com>
2021-05-29 11:38:41 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>Users</button>
+      <br />
+      <br />
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
